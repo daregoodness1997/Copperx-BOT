d9151221274ceb05ff880104d82e4656e254749f
Dare Goodness <daregoodness@gmail.com>
2025-03-25 11:57:55 +0100

    add express framework and configure basic server setup with environment port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { CopperxBot } from "./bot/bot";
+import { EnvConfig } from "./config/env";
+import express, { Request, Response } from "express";
+const app = express();
+const port = EnvConfig.port || 4000;
 
 const bot = new CopperxBot();
 bot.launch();
 bot.setupShutdown();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
